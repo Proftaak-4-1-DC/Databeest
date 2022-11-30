54a0216aa1a699fc2d7e18219a301495e9e0df9e
Niels Eppenhof <n.eppenhof@gmail.com>
2022-11-30 12:14:55 +0100

    Init

---
+++ Databeest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Databeest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
