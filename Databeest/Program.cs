81625cf5e58cd5f3e15cd53546b9f9aa2710a464
Niels <n.eppenhof@gmail.com>
2022-12-13 14:53:00 +0100

    We redirect with nginx

--- Databeest/Program.cs
+++ Databeest/Program.cs
@@ -16,7 +16,7 @@ app.UseForwardedHeaders(new ForwardedHeadersOptions
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    app.UseHttpsRedirection();
+    //app.UseHttpsRedirection();
 
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
