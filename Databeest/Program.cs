302c09ce03345836e4918f2ced6c1812d1666051
Niels Eppenhof <n.eppenhof@gmail.com>
2022-12-13 11:41:14 +0100

    Https shizzle

--- Databeest/Program.cs
+++ Databeest/Program.cs
@@ -23,7 +23,7 @@ if (!app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
