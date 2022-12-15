9ea200edaa27e40a5ffe8e062ba8274bd7e2f71d
Max <maxnouts@gmail.com>
2022-12-15 12:48:59 +0100

    Added first layout version dashboard.

---
+++ Databeest/wwwroot/js/index.js
@@ -0,0 +1,40 @@
+function openHome() {
+    var homePickerNB = document.getElementById("homeOpener");
+    if (homePickerNB.style.display === "block") {
+        homePickerNB.style.display = "none";
+    } else if (homePickerNB.style.display === "none") {
+        homePickerNB.style.display = "block";
+    }
+}
+
+function openWifi() {
+    let wifiPickerNB = document.getElementById("wifiPicker");
+    if (wifiPickerNB.style.display === "block") {
+        wifiPickerNB.style.display = "none";
+    } else if (wifiPickerNB.style.display === "none") {
+        wifiPickerNB.style.display = "block";
+    }
+}
+
+function getTime() {
+    let dateTime = new Date();
+    let date = dateTime.getUTCDate();
+    let month = dateTime.getUTCMonth() + 1;
+    let year = dateTime.getUTCFullYear();
+    let hours = dateTime.getUTCHours() + 1;
+    let minutes = dateTime.getUTCMinutes();
+
+    if (minutes < 10) {
+        let minutesCheck = "0" + minutes;
+        let dateString = date + "/" + month + "/" + year;
+        let timeString = hours + ":" + minutesCheck;
+        document.getElementById("date").innerHTML = dateString;
+        document.getElementById("time").innerHTML = timeString;
+    } else {
+        let dateString = date + "/" + month + "/" + year;
+        let timeString = hours + ":" + minutes;
+        document.getElementById("date").innerHTML = dateString;
+        document.getElementById("time").innerHTML = timeString;
+    }
+}
+getTime();
